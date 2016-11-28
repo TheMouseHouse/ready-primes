41255aae7009b7b9d8690d37644b554db39a80b5
Piotr Skonieczny <piosko@gmail.com>
2016-11-28 19:03:40 +0100

    Fixed path resolve in JsonHelper

--- bin/ready-primes.d.ts
+++ bin/ready-primes.d.ts
@@ -1,48 +1,7 @@
-/// <reference types="bluebird" />
-import * as Promise from 'bluebird';
-
 declare module 'ready-primes' {
-    class ReadyPrimes {
-        private static getMultiple(func, chunks, size, index?);
-        /**
-         * Returns Promise, which when resolved, returns an Array of Primes of length required (size).
-         *
-         * @static
-         * @param {number} size
-         * @param {number} [index=0]
-         * @returns {Promise<any>}
-         *
-         * @memberOf ReadyPrimes
-         */
+    export class ReadyPrimes {
         static primes(size: number, index?: number): Promise<any>;
-        /**
-         * Returns Promise, which when resolved, returns Array with given length (size) of 0's and 1's corresponding to whether index is a Prime or not.
-         * Example:
-         *
-         *         *  *     *     *             *   ... primes
-         * [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 ] ... Indexes
-         * [ 0, 0, 1, 1, 0, 1, 0, 1, 0, 0,  0,  1 ] ... result you will receive.
-         *
-         * Therefore, result Array[11] = 1 = true = prime
-         *
-         * @static
-         * @param {number} size
-         * @param {number} [index=0]
-         * @returns {Promise<any>}
-         *
-         * @memberOf ReadyPrimes
-         */
         static integers(size: number, index?: number): Promise<any>;
-        /**
-         * Returns Promise, which when resolved, returns true or false whether provided number is a Prime.
-         *
-         * @static
-         * @param {number} n
-         * @returns {Promise<any>}
-         *
-         * @memberOf ReadyPrimes
-         */
         static isPrime(n: number): Promise<any>;
     }
-
 }
--- src/helpers/JsonHelper.ts
+++ src/helpers/JsonHelper.ts
@@ -1,78 +1,80 @@
 /// <reference path="../../typings/jsonfile.d.ts" />
 
+import * as path from 'path';
 import * as jsonfile from 'jsonfile';
-import * as fs from 'fs-extra';
+import * as fsExtra from 'fs-extra';
 import * as Promise from 'bluebird';
 import * as _each from 'lodash/each';
 
 export class JsonHelper {
 
-	static INTEGER_EXT: string = '.int';
-	static PRIME_EXT: string = '.prime';
+    static INTEGER_EXT: string = '.int';
+    static PRIME_EXT: string = '.prime';
+    static DATA_PATH: string = path.resolve(__dirname, '../../data');
 
-	static read( filename: string | number ): Promise<any> {
-		return new Promise(( resolve: Function, reject: Function ) => {
-			jsonfile.readFile( `./data/${filename}`, {}, ( err: any, chunk: number[] ) => {
-				if ( err ) {
-					reject( err );
-				} else {
-					resolve( chunk );
-				}
+    static read(filename: string | number): Promise<any> {
+        return new Promise((resolve: Function, reject: Function) => {
+            jsonfile.readFile(`${JsonHelper.DATA_PATH}/${filename}`, {}, (err: any, chunk: number[]) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(chunk);
+                }
 
-			});
-		});
-	}
+            });
+        });
+    }
 
-	static write( filename: string | number, data: any ): Promise<any> {
-		return new Promise(( resolve: Function, reject: Function ) => {
-			jsonfile.writeFile( `./data/${filename}`, data, {}, ( err: any ) => {
-				if ( err ) {
-					console.log( err );
-					reject( false );
-				} else {
-					resolve( true );
-				}
-			});
-		});
-	}
+    static write(filename: string | number, data: any): Promise<any> {
+        return new Promise((resolve: Function, reject: Function) => {
+            jsonfile.writeFile(`${JsonHelper.DATA_PATH}/${filename}`, data, {}, (err: any) => {
+                if (err) {
+                    console.log(err);
+                    reject(false);
+                } else {
+                    resolve(true);
+                }
+            });
+        });
+    }
 
-	static readIntegerFile( filename: string | number ): Promise<any> {
-		return JsonHelper.read( filename + JsonHelper.INTEGER_EXT );
-	}
+    static readIntegerFile(filename: string | number): Promise<any> {
+        return JsonHelper.read(filename + JsonHelper.INTEGER_EXT);
+    }
 
-	static readMultipleIntegerFiles( filenames: number[] | string[] ): Promise<any> {
-		let promises: Promise<any>[] = [];
-		_each( filenames, ( filename: number | string ) => {
-			promises.push( JsonHelper.readIntegerFile( filename ) );
-		});
-		return Promise.all( promises );
-	}
+    static readMultipleIntegerFiles(filenames: number[] | string[]): Promise<any> {
+        let promises: Promise<any>[] = [];
+        _each(filenames, (filename: number | string) => {
+            promises.push(JsonHelper.readIntegerFile(filename));
+        });
+        return Promise.all(promises);
+    }
 
-	static writeIntegerFile( filename: string | number, data: any ): Promise<any> {
-		return JsonHelper.write( filename + JsonHelper.INTEGER_EXT, data );
-	}
+    static writeIntegerFile(filename: string | number, data: any): Promise<any> {
+        return JsonHelper.write(filename + JsonHelper.INTEGER_EXT, data);
+    }
 
-	static readPrimeFile( filename: string | number ): Promise<any> {
-		return JsonHelper.read( filename + JsonHelper.PRIME_EXT );
-	}
+    static readPrimeFile(filename: string | number): Promise<any> {
+        return JsonHelper.read(filename + JsonHelper.PRIME_EXT);
+    }
 
-	static readMultiplePrimeFiles( filenames: number[] | string[] ): Promise<any> {
-		let promises: Promise<any>[] = [];
-		_each( filenames, ( filename: number | string ) => {
-			promises.push( JsonHelper.readPrimeFile( filename ) );
-		});
-		return Promise.all( promises );
-	}
+    static readMultiplePrimeFiles(filenames: number[] | string[]): Promise<any> {
+        let promises: Promise<any>[] = [];
+        _each(filenames, (filename: number | string) => {
+            promises.push(JsonHelper.readPrimeFile(filename));
+        });
+        return Promise.all(promises);
+    }
 
-	static writePrimeFile( filename: string | number, data: any ): Promise<any> {
-		return JsonHelper.write( filename + JsonHelper.PRIME_EXT, data );
-	}
+    static writePrimeFile(filename: string | number, data: any): Promise<any> {
+        return JsonHelper.write(filename + JsonHelper.PRIME_EXT, data);
+    }
 
-	static writeReference( data: any ): void {
-		const referencePath: string = './data/reference.js';
-		const referenceData: string = 'exports.reference = ' + JSON.stringify( data );
+    static writeReference(data: any): void {
+        const referencePath: string = `${JsonHelper.DATA_PATH}/reference.js`;
+        const referenceData: string = 'exports.reference = ' + JSON.stringify(data);
 
-		fs.outputFile( referencePath, referenceData );
-	}
+        fsExtra.outputFile(referencePath, referenceData);
+    }
 
 }
